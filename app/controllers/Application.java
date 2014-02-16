90e6a904e0d7bd9993d7e6ff565378d3ea70e3b3
Samuel Coelho <samuelfcmc@gmail.com>
2014-02-16 18:11:36 +0000

    Implemented the behaviour after the user log in

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,36 @@
 package controllers;
 
+import models.User;
+import fenixedu.FenixEduClientQAFactory;
 import play.*;
 import play.mvc.*;
-
+import pt.ist.fenixedu.sdk.FenixEduClient;
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Hello world", "Test"));
+    	String name = "";
+    	String username = session("user");
+    	
+    	String code = request().getQueryString("code");
+    	if(code != null && username == null) {
+    		User user = User.authenticate(code);
+    		name = user.name;
+    		session().clear();
+    		session("user", user.userName);
+    	}
+    	else if(username != null) {
+    		User user = User.findByUsername(username);
+    		if(user == null) {
+    			session().clear();
+    		}
+    		else {
+    			name = user.name;
+    		}
+    		
+    	}
+        return ok(index.render("Hello world", name));
     }
     
     public static Result questionAsk() {
