3424c266aa07b0cc52388cc7b5e2462fe7bb924e
Samuel Coelho <samuelfcmc@gmail.com>
2014-02-16 18:10:49 +0000

    Added a find by username method and authenticate

--- app/models/User.java
+++ app/models/User.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -8,7 +9,11 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
+import fenixedu.FenixEduClientQAFactory;
 import play.db.ebean.Model;
+import pt.ist.fenixedu.sdk.FenixEduClient;
+import pt.ist.fenixedu.sdk.FenixEduUserConfig;
+import pt.ist.fenixedu.sdk.beans.FenixPerson;
 
 @Entity
 public class User extends Model {
@@ -35,6 +40,57 @@ public class User extends Model {
 	
 	@ManyToMany(cascade=CascadeType.PERSIST)
 	public List<Course> courses;
+	
+	public static Finder<String,User> find = new Finder<String,User>(
+	        String.class, User.class); 
+	
+	
+	
+	public User(String name, String userName, String accessToken,
+			String refreshToken) {
+		super();
+		this.name = name;
+		this.userName = userName;
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+		this.questions = new ArrayList<Question>();
+		this.answers = new ArrayList<Answer>();
+		this.courses = new ArrayList<Course>();
+	}
+
+	public FenixEduUserConfig getFenixEduUserConfig() {
+		FenixEduUserConfig config = new FenixEduUserConfig(userName, accessToken, refreshToken);
+		return config;
+	}
+	
+	public static User authenticate(String code) {
+		FenixEduClient client = FenixEduClientQAFactory.getSingleton();
+		FenixEduUserConfig userConfig = new FenixEduUserConfig();
+		
+		client.setCode(code, userConfig);
+		
+		FenixPerson person = client.getPerson(userConfig);
+		
+		User user = findByUsername(person.getUsername());
+		
+		if(user == null) {
+			user = new User(person.getName(), person.getUsername(), userConfig.getAccessToken(), userConfig.getRefreshToken());
+		}
+		else {
+			user.accessToken = userConfig.getAccessToken();
+			user.accessToken = userConfig.getRefreshToken();
+		}
+		
+		user.save();
+		
+		return user;		
+	}
+	
+	public static User findByUsername(String username) {
+		User user = find.where().eq("userName", username).findUnique();
+		
+		return user;
+	}
 	
 	
 	
