bb345155f690effdbf72666ded4733ac0b8b952a
Samuel Coelho <samuelfcmc@gmail.com>
2014-02-20 23:14:24 +0000

    Implemented behaviour of tags text input in question ask action

--- app/controllers/Tags.java
+++ app/controllers/Tags.java
@@ -1,10 +1,12 @@
 package controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import models.Question;
 import models.Tag;
 import models.User;
+import play.api.libs.json.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security.Authenticated;
@@ -25,6 +27,16 @@ public class Tags extends Controller {
 		}
 		
 		return ok(listQuestions.render("Questions from personal tags: ", selectedQuestions));
-
+	}
+	
+	public static Result getAllTags() {
+		List<Tag> tags = Tag.find.all();
+		String result = "";
+		
+		for(Tag tag : tags) {
+			result += tag.label + ",";
+		}
+		
+		return ok(result);
 	}
 }
